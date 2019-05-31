6e37555d7b02e5e467746dda8a459f55a6314808
Lucas Aquiles <lucasaquiles@gmail.com>
2019-05-31 13:16:45 -0300

    ajuste de dependencias

--- src/main/kotlin/com/lucasaquiles/Application.kt
+++ src/main/kotlin/com/lucasaquiles/Application.kt
@@ -1,4 +1,4 @@
-package com.lucasaquies
+package com.lucasaquiles
 
 import io.micronaut.runtime.Micronaut
 
@@ -7,7 +7,7 @@ object Application {
     @JvmStatic
     fun main(args: Array<String>) {
         Micronaut.build()
-                .packages("com.lucasaquies")
+                .packages("com.lucasaquiles")
                 .mainClass(Application.javaClass)
                 .start()
     }
