ce7738c95a8052b8c57aa5efa0278178eb0e27f0
Lucas Aquiles <lucasaquiles@gmail.com>
2019-12-16 16:02:03 -0300

    update configs

--- src/main/kotlin/com/lucasaquiles/Application.kt
+++ src/main/kotlin/com/lucasaquiles/Application.kt
@@ -4,7 +4,15 @@ import io.micronaut.runtime.Micronaut
 import io.swagger.v3.oas.annotations.OpenAPIDefinition
 
 
-@OpenAPIDefinition
+@OpenAPIDefinition(
+        info = @Info(
+                title = "Quiz Manager API",
+                version = "0.1",
+                description = "sample project using micronaut",
+                license = @License(name = "Apache 2.0"),
+                contact = @Contact(name = "@lucasaquiles")
+        )
+)
 object Application {
 
     @JvmStatic
